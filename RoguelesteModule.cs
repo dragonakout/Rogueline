20509447a50b518410f805ea12f831b0a2889d2e
Maigan Periot <maigan.periot@yahoo.ca>
2022-06-24 15:46:39 -0400

    initial commit

---
+++ RoguelesteModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Celeste.Mod.Rogueleste {
+    public class RoguelesteModule : EverestModule {
+        
+        public static RoguelesteModule Instance;
+
+        public RoguelesteModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+
+    }
+}
